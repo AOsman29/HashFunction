69776eb940d919b036da5651cd55a8a6b0ec7775
Abdalla Osman <99369800+AOsman29@users.noreply.github.com>
2024-02-24 14:28:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
 CSE 310 Hash Function DIY Contest
 Instructor: Yiran "Lawrence" Luo
-Your name(s): Abdalla Osman. Breck
-Your team alias: 
+Your name(s): Abdalla Osman, Breck
+Your team alias: ChatDjb2
 */
 
 #include <iostream>
